2f3b9b2633f9de9b15a52bfbc29046a0c61c82de
deepakramoham <145920972+deepakramoham@users.noreply.github.com>
2025-07-24 01:53:05 +0530

    completed deployement settings

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
+const { connectDB, connectDBLocal } = require("./config/dbConn");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/fleetlink");
-
+//mongoose.connect("mongodb://localhost:27017/fleetlink");
+connectDB();
+//connectDBLocal();
 app.use("/api/vehicles", require("./routes/vehicle"));
 app.use("/api/bookings", require("./routes/booking"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
